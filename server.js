ec147b3e3937cd2f755bc76021b24cbf59c6fe33
mona <mona27feb@rediffmail.com>
2018-01-21 14:07:07 +0530

    inistal

---
+++ server.js
@@ -0,0 +1,18 @@
+let app = require('express')();
+
+app.get('/',function (req,res) {
+
+    res.json('Hi, Hope u r doing good')
+
+});
+
+
+
+app.listen('3000',function(err){
+
+   if(!err){
+       console.log('app connected')
+   }else{
+       console.log(err)
+   }g
+});
